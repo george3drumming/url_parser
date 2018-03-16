c1f969d3830e63d43beb6d69f9d607ccfa68d98c
George Drumming <george3drumming@gmail.com>
2018-03-16 21:16:57 +0000

    Intial Commit for URL PARSERS

---
+++ url_parser.rb
@@ -0,0 +1,60 @@
+# go to https://docs.ruby-lang.org/en/2.1.0/URI.html
+#url module for ruby
+
+class UrlParser 
+    attr_reader :scheme, :domain, :path, :fragment_id
+
+    def initialize(url)
+        @url = url
+        @scheme = @url.split('/')[0].split(':')[0]
+        @domain = @url.split('/')[2].split(':')[0] 
+        if @url.split('/')[2].include? ':' 
+            @port = @url.split('/')[2].split(':')[1]
+        else 
+            @port = nil 
+        end
+        @path = @url.split('/')[3].split('?')[0]
+        @querystring = {}
+    end
+
+    def query_string
+        qs = @url.split('/')[3].split('?')[1].split('#')[0].split('&')
+        qs.each do |item|
+            param = item.split('=')
+            @querystring[param[0]] = param[1]
+        end 
+        @querystring
+    end
+
+    def path 
+        if @path == "" 
+            @path = nil
+        end 
+        @path
+    end
+
+    def port 
+        if (@port == "" || @port == nil) && scheme == "https"
+            @port = 443.to_s
+        elsif (@port == "" || @port == nil) && scheme == "http"
+            @port = 80.to_s
+        end 
+        @port
+    end
+
+    def fragment_id
+        @fragment_id = @url.split('/')[3].split('?')[1].split('#')[1]
+    end
+end    
+    #  github_url.scheme
+#     => "https"
+#     github_url.domain
+#     => "github.com"
+#     github_url.port
+#     => "443"
+#     github_url.path
+#     => "search"
+#     github.query_string
+#     => {"q" => "ruby"}
+#     github_url.fragment_id
+#     => "stuff"
